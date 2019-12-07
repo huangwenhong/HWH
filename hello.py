b3b23fdbf564357766282d782c56fae5d88259ff
hello <996778961@qq.com>
2019-12-07 18:04:42 +0800

    add hello

--- hello.py
+++ hello.py
@@ -1,3 +1,15 @@
-1111111111111111111111111
+print ("你好！")
+print ("hello")
 
-3333333333333333333333333
+public class test_11 {
+    public static void main(String[] args) {
+        int a = 0;
+        if (a < 1) {
+            System.out.println(false);
+            a++;
+        }else {
+            
+        }
+        System.out.println(true);
+    }
+}
